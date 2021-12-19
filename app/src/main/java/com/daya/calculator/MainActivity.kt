8d62d4910dff2a8b498df59b861d1be6d691aaa3
ad <da>
2021-12-19 15:21:09 +0700

    transparent status bar

--- app/src/main/java/com/daya/calculator/MainActivity.kt
+++ app/src/main/java/com/daya/calculator/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.daya.calculator
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.FrameLayout
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.daya.calculator.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayoutMediator
 
@@ -14,6 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        overlapStatusBar()
 
         binding.vp2.apply {
             adapter = ScreenSliderAdapter(this@MainActivity)
@@ -25,5 +31,27 @@ class MainActivity : AppCompatActivity() {
                 else -> throw IllegalStateException("cannot find icon position")
             }
         }.attach()
+    }
+
+    private fun overlapStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, windowInsets ->
+
+                val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+                view.layoutParams = (view.layoutParams as FrameLayout.LayoutParams).apply {
+                    leftMargin = insets.left
+                    bottomMargin = insets.bottom
+                    rightMargin = insets.right
+                }
+                WindowInsetsCompat.CONSUMED
+            }
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
+        }
+
+
     }
 }
